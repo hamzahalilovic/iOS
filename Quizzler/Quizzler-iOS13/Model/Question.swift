79d30c2f3ef11266d088450e6db2ec4299538c58
Hamza Halilovic <hamzahalilovic.12@gmail.com>
2020-07-16 12:40:46 +0200

    Quizzler app

---
+++ Quizzler/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Hamza Halilovic on 15/07/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
