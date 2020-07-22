8d6b9275c559d1c16db105f9d39dab3b7086b17d
Hamza Halilovic <hamzahalilovic.12@gmail.com>
2020-07-22 10:09:25 +0200

    Flash chat app

---
+++ FlashChat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -0,0 +1,153 @@
+//
+//  ChatViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ChatViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = [
+        Message(sender: "123@gmail.com", body: "hey!"),
+        Message(sender: "gmail@gmail.com", body: "How are you?"),
+        Message(sender: "mail@mail.com", body: "Hello!")
+        
+        
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //       tableView.delegate = self
+        tableView.dataSource = self
+        title = Constants.appName //set navigation title
+        navigationItem.hidesBackButton = true //hide back button
+        tableView.register(UINib(nibName: Constants.cellNibName, bundle: nil), forCellReuseIdentifier: Constants.cellIdentifier)
+        
+        loadMessages()
+    }
+    
+    func loadMessages() {
+        db.collection(Constants.FStore.collectionName)
+            .order(by: Constants.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+            
+            self.messages = []
+
+            
+            if let e = error {
+                print("There was an issue retrieving data, \(e)")
+            }else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments{
+                        let data = doc.data()
+                        if let messageSender = data[Constants.FStore.senderField] as? String, let messageBody = data[Constants.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async{
+                                 self.tableView.reloadData()
+                                //scroll to last message when sending
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    }
+    @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(Constants.FStore.collectionName)
+                .addDocument(data: [
+                    Constants.FStore.senderField: messageSender,
+                    Constants.FStore.bodyField: messageBody,
+                    Constants.FStore.dateField: Date().timeIntervalSince1970
+                    ])
+                { (error) in
+                if let e = error {
+                    print ("therre was issuse saving to firestore, \(e)")
+                }else{
+                    print("Sucessful saving")
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    
+    //  LOG OUT USER
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        
+        do {
+            try Auth.auth().signOut()
+            // when log out bring user back to root navigation controller
+            navigationController?.popToRootViewController(animated: true)
+            
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+    }
+}
+
+
+extension ChatViewController: UITableViewDataSource {
+    //number of cells
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    //create cells
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath )
+           as! MessageCell
+           cell.label.text = message.body
+        
+        //message from current user
+        if message.sender == Auth.auth().currentUser?.email{
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: Constants.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: Constants.BrandColors.purple)
+            
+        }
+            //message form other user
+        else{
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: Constants.BrandColors.purple)
+            cell.label.textColor = UIColor(named: Constants.BrandColors.lightPurple)
+            
+        }
+        
+   
+        return cell
+    }
+    
+    
+    
+}
+//interact with cells
+//extension ChatViewController: UITableViewDelegate {
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//
+//}
