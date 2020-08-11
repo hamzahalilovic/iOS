8f9a4f28ffe9ce1d1218d6fe34d55473de4c554a
Hamza <hamzahalilovic.12@gmail.com>
2020-08-11 19:58:56 +0200

    Inceptionv3.mlmodel

--- FoodScan - CoreML food scan app/FoodScan/ViewController.swift
+++ FoodScan - CoreML food scan app/FoodScan/ViewController.swift
@@ -37,10 +37,53 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
            
             //display picture user picked in the UIImageView
                    imageView.image = userPickedImage
+            
+            //convert UIImage to CIImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("could not convert UIImage to CIImage")
+            }
+            
+            //run ciimage through func detect() method
+            detect(image: ciimage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
        
+    }
+    
+    func detect(image: CIImage) {
+        
+        //gurad serves for if model == nil trigger else statement
+        //VNCoreMLModel comes from Vision
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("loading CoreML model failed")
+        }
+        
+        //vision CoreML request
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            //process results of request
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("model failed to process image")
+            }
+            
+            //if successful
+            print(results)
+            
+        }
+        
+        //handler to specify the image to classify
+        //image comes from uiimage which is converted to ciimage which goes to func detect()
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }catch {
+            print(error)
+        }
+        
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
